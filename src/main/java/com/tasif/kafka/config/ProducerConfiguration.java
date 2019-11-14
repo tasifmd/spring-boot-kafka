3616699fb4fbdfe7406d5df0d3325b62aaab79ae
tasifmd <tasif.mohammed@indusnet.co.in>
2019-11-14 14:23:08 +0530

    kafka producer and consumer done

--- src/main/java/com/tasif/kafka/config/ProducerConfiguration.java
+++ src/main/java/com/tasif/kafka/config/ProducerConfiguration.java
@@ -20,7 +20,6 @@ public class ProducerConfiguration {
 	@Bean
 	public ProducerFactory<String, User> producerFactory() {
 		Map<String, Object> config = new HashMap<>();
-
 		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
